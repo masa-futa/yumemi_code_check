d29e8756586c7ccb300b1c967c6db1a61e0c5bbe
masa-futa <masakunn1215@gmail.com>
2024-10-25 00:57:53 +0900

    [change] ライトモード / ダークモードのテーマを指定

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yumemi_code_check/feature/search/search_screen.dart';
+import 'package:yumemi_code_check/thema/custom_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final customTheme = ref.watch(customThemeProvider);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SizedBox.shrink(),
+      theme: customTheme.lightThemeData(context),
+      darkTheme: customTheme.darkThemeData(context),
+      home: const SearchScreen(),
     );
   }
 }
