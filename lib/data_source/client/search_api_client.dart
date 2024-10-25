6000413569a62212f86385ef83c522d08eec255f
masa-futa <masakunn1215@gmail.com>
2024-10-25 21:58:23 +0900

    [add] api clientを新規作成

---
+++ lib/data_source/client/search_api_client.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_code_check/data_source/api_exception.dart';
+import 'package:yumemi_code_check/data_source/config/search_config.dart';
+import 'package:yumemi_code_check/model/search_model.dart';
+import 'package:yumemi_code_check/query_service/query_service.dart';
+import 'package:yumemi_code_check/utils/result.dart';
+
+part 'generated/search_api_client.g.dart';
+
+@riverpod
+Future<void> getSearchRepositories(
+  Ref ref, {
+  required String token,
+  required String keyword,
+  required QueryService queryService,
+}) async {
+  final config = ref.read(searchConfigProvider);
+  try {
+    final response = await http.get(
+      Uri.https(config.host, config.path, config.query?.get(keyword)),
+      headers: config.header(token),
+    );
+
+    if (response.statusCode > 209) {
+      final exception =
+          ref.read(apiExceptionProvider(statucCode: response.statusCode));
+      queryService.subscribe(Failure(exception, StackTrace.empty));
+      return;
+    }
+    final decoded = json.decode(response.body);
+    final searchModel = SearchModel.fromJson(decoded);
+    queryService.subscribe(Success(searchModel));
+  } on ApiException catch (e, s) {
+    queryService.subscribe(Failure(e, s));
+  }
+}
---
+++ lib/data_source/client/token_api_client.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_code_check/data_source/api_exception.dart';
+import 'package:yumemi_code_check/data_source/config/token_config.dart';
+import 'package:yumemi_code_check/model/token_model.dart';
+import 'package:yumemi_code_check/utils/result.dart';
+
+part 'generated/token_api_client.g.dart';
+
+@riverpod
+Future<Result<String, ApiException>> getToken(Ref ref) async {
+  final config = ref.read(tokenConfigProvider);
+
+  try {
+    final response = await http.post(
+      Uri.https(config.host, config.path),
+      headers: config.header(),
+    );
+    if (response.statusCode > 209) {
+      final exception =
+          ref.read(apiExceptionProvider(statucCode: response.statusCode));
+      return Failure(exception, StackTrace.empty);
+    }
+
+    final decoded = json.decode(response.body);
+    final token = TokenModel.fromJson(decoded).token;
+    return Success(token);
+  } on ApiException catch (e, s) {
+    return Failure(e, s);
+  }
+}
