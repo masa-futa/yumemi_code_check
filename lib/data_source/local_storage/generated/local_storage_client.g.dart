828c8eac48832b01785b67edeac487526ff998f8
masa-futa <masakunn1215@gmail.com>
2024-10-27 07:55:24 +0900

    [add] shared_preferences追加
    デバッグ機能で用いるため

---
+++ lib/data_source/local_storage/generated/local_storage_client.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../local_storage_client.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localStorageClientHash() =>
+    r'0fc882feff1363f10c6e0ed23530936ffb4d161a';
+
+/// See also [localStorageClient].
+@ProviderFor(localStorageClient)
+final localStorageClientProvider =
+    AutoDisposeProvider<LocalStorageClient>.internal(
+  localStorageClient,
+  name: r'localStorageClientProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localStorageClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef LocalStorageClientRef = AutoDisposeProviderRef<LocalStorageClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
